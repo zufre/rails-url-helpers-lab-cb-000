21b8576934eee08397d8a4e1689c959c31929ede
Cedric David <cedric.david@btopenworld.com>
2018-09-25 03:48:35 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,5 +1,5 @@
 class AddActiveToSTudents < ActiveRecord::Migration
   def change
-    add_column :student, :active, :boolean, default :false
+    add_column :students, :active, :boolean, default: false
   end
 end
