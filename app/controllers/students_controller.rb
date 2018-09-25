4cef8f65917c958a5bce740aafc5bdd2a4ae6796
Cedric David <cedric.david@btopenworld.com>
2018-09-25 03:36:03 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,13 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
   end
-  
+
   def activate
     set_student.toggle! :active
 
