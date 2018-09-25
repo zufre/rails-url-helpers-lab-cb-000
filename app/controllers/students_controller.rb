4b667fb50d9276af035e92dc624cebdf7af5df3a
Cedric David <cedric.david@btopenworld.com>
2018-09-25 03:35:55 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,6 +7,12 @@ class StudentsController < ApplicationController
 
   def show
   end
+  
+  def activate
+    set_student.toggle! :active
+
+    redirect_to student_path(@student)
+  end
 
   private
 
